2e6dc7fb9cbe12f5284bd2f18a2c19061d2e2cd8
Daichi410musk <dachi410musk@gmail.com>
2025-11-16 01:13:27 +0900

    Implement article show API (Task.7-4)

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class BaseApiController < ApplicationController
+      include DeviseTokenAuth::Concerns::SetUserByToken
+
+      before_action :authenticate_user!
     end
   end
 end
